4b7e95470fb34e235f39d2622c6c40c698502b32
Chirag Kalra <chiragkalra640@gmail.com>
2024-02-29 01:38:22 +0530

    changing port

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import express from "express";
 const app = express();
-const PORT = 8000; 
+const port = Number(process.env.PORT) || 8000; 
 import bodyParser from "body-parser";
 
 import { translateController } from "./controller/controller.ts";
@@ -11,6 +11,6 @@ app.post("/translateEnFr", async (req, res) => {
   await translateController(req, res);
 });
 
-app.listen(PORT,"0.0.0.0", () => {
-  console.log(`server is running at port ${PORT}`);
+app.listen(port,"0.0.0.0", () => {
+  console.log(`server is running at port ${port}`);
 });
